ca8dce0e6ce199f2b5b3f4e09bec40df4d670dd4
Aarya Patil <aaryapatil402@gmail.com>
2024-09-04 15:58:53 +0530

    Fixes

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import app
-#from setup_ffmpeg import setup_ffmpeg
+from setup_ffmpeg import setup_ffmpeg
 
 if __name__ == "__main__":
-  #  setup_ffmpeg()
+    setup_ffmpeg()
     app.run(debug=True)
